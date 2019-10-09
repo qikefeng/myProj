eed6ea1387e0ebe48b59413d6e3db7757b47de51
qikefeng <513248692@qq.com>
2019-10-09 10:34:43 +0800

    头部html结构

--- SHOPStore/client/src/js/index.js
+++ SHOPStore/client/src/js/index.js
@@ -1,12 +1,18 @@
 import "../less/normalized.less";
 import "../less/public.less";
 import "../less/index.less";
+import "../less/head.less";
+
 import { BASE_URL } from "./lib";
 
 $(function(){
     $(".headlogo").html(`<img src="${BASE_URL}/images/sony-logo.jpg">`)
-
-
+    
 
 
 })
+
+
+
+
+
